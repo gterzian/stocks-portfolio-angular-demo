eabedec8a82ed6323acc017c7492f308483eecb2
Gregory <gregory.terzian@gmail.com>
2014-03-18 19:14:10 +0800

    proper encapsulation of model logic into front end portfolio

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -3,47 +3,66 @@ var app = angular.module('myApp', []);
 app.factory('portfolio', function($http, $q) {
 	
 	var portfolio = {};
-	var deferred = $q.defer();
 	
-	$http.get('user/').success(function(data, status, headers, config) {
-			         portfolio.cash_balance = data.cash_balance;
-					 portfolio.user_id = data.id
-					 portfolio.purchases = data.purchases;
-					 deferred.resolve(portfolio)
+	var init = function () {
+		var deferred = $q.defer();
+	
+		$http.get('user/').success(function(data, status, headers, config) {
+				         this.cash_balance = data.cash_balance;
+						 this.user_id = data.id
+						 this.purchases = data.purchases;
+						 deferred.resolve(this)
 					 
-		        }); 
+			        }); 
 			
-	return deferred.promise
+		return deferred.promise
+	}
 	
+	var place_order = function (order) {
+	  var order_deferred = $q.defer();	
+	  $http.post('/new_transaction/', order).success(
+		    function (portfolio) {
+				    if (portfolio.success) {
+					    
+						this.cash_balance = portfolio.cash_balance 
+						this.purchases = portfolio.purchases;
+						order_deferred.resolve(this)
+						
+				    }
+				    
+				   }
+
+	        );
+	   return  order_deferred.promise
+	}
+	
+	portfolio.init = init
+	portfolio.place_order = place_order
+	return portfolio
       
 });
 			
 			
 app.controller('PortfolioController', function($scope, $http, portfolio) {
-	portfolio.then(function(portfolio){
+	portfolio.init().then(function(portfolio){
 		$scope.cash_balance = portfolio.cash_balance 
 		$scope.purchases = portfolio.purchases;
 	})
 	$scope.stock = {ticker: '', units: ''};
 	
 	var transact = function (order) {
-		
-	  $http.post('/new_transaction/', order).success(
-		    function (portfolio) {
-				    if (portfolio.success) {
-					    
-						$scope.cash_balance = portfolio.cash_balance 
-						$scope.purchases = portfolio.purchases;
-						$scope.message = "you've just transacted for " + order.units + ' units of ' + order.ticker + ' (' + order.type + ') '
-						$scope.stock = {ticker: '', units: ''};
-				    }
-					else {
-						$scope.message = 'sorry, your order couldn"t be placed'
-					}
-				    
-				   }
-
-	        );
+		portfolio.place_order(order).then(function(portfolio){
+		    if (portfolio.success) {
+		    
+				$scope.cash_balance = portfolio.cash_balance 
+				$scope.purchases = portfolio.purchases;
+				$scope.message = "you've just transacted for " + order.units + ' units of ' + order.ticker + ' (' + order.type + ') '
+				$scope.stock = {ticker: '', units: ''};
+		    }
+			else {
+				$scope.message = 'sorry, your order couldn"t be placed'
+			}
+		})
 	}
 
  	$scope.requestQuote = function () {
